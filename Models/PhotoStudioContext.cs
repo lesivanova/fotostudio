20962ec0caee246ac662b2925a332288f4c5c8c8
olesya <olesyaivanova03@gmail.com>
2023-12-24 23:14:00 +0300

    firstCommit

---
+++ Models/PhotoStudioContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoStudio.Model
+{
+    class PhotoStudioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Data Source=LAPTOP-JK32SL7A\SQLEXPRESS
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-13OO92Q\SQLEXPRESS01; Initial Catalog=photostudiodb; Integrated Security=True"); 
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Service> Services { get; set; }
+    }
+}
